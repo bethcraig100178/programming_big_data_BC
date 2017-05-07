5927fa6a75d811c0bedcd971fd50f9b8477e95ef
bethcraig100178 <10331736@mydbs.ie>
2017-05-07 22:47:44 +0100

    Fixed problem with comment line. Was getting comment line for next commit. Got test file working. More tests to be added

--- CA4/CA4.py
+++ CA4/CA4.py
@@ -7,23 +7,24 @@
 
 #create alias for the file
 
-file = 'changes_python.log'
+file = 'changes_python.txt'
 
 class Commit(object): #create a class for commits
 
     '''CLass Commit'''
     
-    def __init__(self, revision = None, author = None, date = None, dayOfTheWeek = None, comment_line_count = None, changes = None, comment = None, numberfiles = None):
+    def __init__(self, revision = None, author = None, date = None, dayOfTheWeek = None, month = None, comment_line_count = None, changes = None, comment = None, numberfiles = None):
         self.revision = revision
         self.author = author
         self.date = date
         self.dayOfTheWeek = dayOfTheWeek
+        self.month = month
         self.comment_line_count = comment_line_count
         self.changes = changes
         self.comment = comment
         self.numberfiles = numberfiles
 
-class MineForObjects(object): #create a class to store output from mining the file
+class Mine_for_Commits(object): #create a class to store output from mining the file
 
     ''' Class to store objects resulting from mining a file'''
     
@@ -38,6 +39,11 @@ class MineForObjects(object): #create a class to store output from mining the fi
         self.numberD = {}
         self.numberR = {}
         self.days_atleastone_commit = {}
+        self.Jul = {}
+        self.Aug = {}
+        self.Sep = {}
+        self.Oct = {}
+        self.Nov = {}
         self.Mon = {}
         self.Tue = {}
         self.Wed = {}
@@ -58,16 +64,18 @@ class MineForObjects(object): #create a class to store output from mining the fi
                 current_commit.author = details[1].strip()
                 current_commit.date = details[2].strip()
                 current_commit.dayOfTheWeek = current_commit.date.split()[3][1:4]
+                current_commit.month = current_commit.date.split()[5]
                 current_commit.comment_line_count = int(details[3].strip().split(' ')[0])
                 current_commit.changes = self.data[index+2:self.data.index('', index+1)]
                 current_commit.numberfiles = len(self.data[index+3:self.data.index('', index+1)])
-                current_commit.comment = self.data[index - current_commit.comment_line_count:index]
                 index = self.data.index(sep, index + 1)
+                current_commit.comment = self.data[index - current_commit.comment_line_count:index]
                 self.commits.append(current_commit)
             except IndexError:
                 break
         
-       
+    
+    
     def commits_per_author(self):
         #Create dictionary to count number of commits per author
         for i in range(len(self.commits)):
@@ -144,27 +152,39 @@ class MineForObjects(object): #create a class to store output from mining the fi
         
         print 'total commits by day', totalByDay
         
-       
+    def month_per_author(self):
+        #Statistic 4 Count number of commits by author and day of the week
+        totalByMonth = 0
+        totalJul = 0
+        totalAug = 0
+        totalSep = 0
+        totalOct = 0
+        totalNov = 0
+        # iterate through commits to find Jul, Aug etc
+        for i in range(len(self.commits)):
+            if self.commits[i].month == 'Jul':
+                totalJul = totalJul + 1
+                self.Jul[self.commits[i].author] = self.Jul.get(self.commits[i].author, 0) + 1 
+            elif self.commits[i].month == 'Aug':
+                totalAug = totalAug + 1
+                self.Aug[self.commits[i].author] = self.Aug.get(self.commits[i].author, 0) + 1 
+            elif self.commits[i].month == 'Sep':
+                totalSep = totalSep + 1
+                self.Sep[self.commits[i].author] = self.Sep.get(self.commits[i].author, 0) + 1    
+            elif self.commits[i].month == 'Oct':
+                totalOct = totalOct + 1
+                self.Oct[self.commits[i].author] = self.Oct.get(self.commits[i].author, 0) + 1 
+            elif self.commits[i].month == 'Nov':
+                totalNov = totalNov + 1
+                self.Nov[self.commits[i].author] = self.Nov.get(self.commits[i].author, 0) + 1 
+        totalByMonth =  totalJul + totalAug + totalSep + totalOct + totalNov  #sanity check
         
-trial = MineForObjects(file)
-trial.add_commits()
-print len(trial.commits)
-trial.commits_per_author()
-print 'the number of commits per author', trial.authors
-trial.files_per_author()
-print 'the number of files per authoer', trial.numberfiles
-trial.amd_per_author()
-print 'the number of adds per author', trial.numberA
-print 'the number of deletes per author', trial.numberD
-print 'the number of modifies per author', trial.numberM
-print 'the number of R per author', trial.numberR
-trial.unique_days_commit()
-print 'number of days at least one commit was made', trial.days_atleastone_commit
-trial.dayOfTheWeek_per_author()
-print 'commits on a monday', trial.Mon
-print 'commits on a tuesday', trial.Tue
-print 'commits on a wed', trial.Wed
-print 'commits on a thur', trial.Thu
-print 'commits on a fri', trial.Fri
+        print 'total commits by month', totalByMonth 
+
+
+ 
+        
+        
+
 
 
--- CA4/test_CA4.py
+++ CA4/test_CA4.py
@@ -1,30 +1,53 @@
 
 import unittest
-from CA4_010517 import *
+from CA4 import *
 
 class TestCommits(unittest.TestCase):
 
     def setUp(self):
-        self.data = self.read_data()
-        self.commits = self.data.get_Commits() 
-
+        self.trial = Mine_for_Commits(file)
+        self.trial.add_commits()
+        self.trial.commits_per_author()
+        self.trial.files_per_author()
+        self.trial.amd_per_author()
+        self.trial.unique_days_commit()
+        self.trial.dayOfTheWeek_per_author()
+        self.trial.month_per_author()
+        
+    
     def test_number_of_lines(self):
-        self.assertEqual(5255, len(self.data))
+        self.assertEqual(5255, len(self.trial.data))
 
     def test_number_of_commits(self):
-        
-        self.assertEqual(422, len(commits))
+        self.assertEqual(422, len(self.trial.commits))
 
     def test_first_commit(self):
-       
-        self.assertEqual('Thomas', commits[0]['author'])
-        self.assertEqual('r1551925', commits[0]['revision'])
-        self.assertEqual(' ', commits[0]['date'])
-        self.assertEqual('', commits[0]['dayOfTheWeek'])
-        self.assertEqual(' ', commits[0]['comment_line_count'])
-        self.assertEqual(' ', commits[0]['changes']) 
-        self.assertEqual(' ', commits[0]['comment'])
-        self.assertEqual(' ', commits[0]['numberfies']) 
+        self.assertEqual('Thomas', self.trial.commits[0].author)
+        self.assertEqual('r1551925', self.trial.commits[0].revision)
+        self.assertEqual('2015-11-27 16:57:44 +0000 (Fri, 27 Nov 2015)', self.trial.commits[0].date)
+        self.assertEqual('Fri', self.trial.commits[0].dayOfTheWeek)
+        self.assertEqual('Nov', self.trial.commits[0].month)
+        self.assertEqual(1, self.trial.commits[0].comment_line_count)
+        self.assertEqual(['Changed paths:', 'M /cloud/personal/client-international/android/branches/android-15.2-solutions/settings.gradle'], self.trial.commits[2].changes) 
+        self.assertEqual(['Renamed folder to the correct name'], self.trial.commits[0].comment)
+        self.assertEqual(4, self.trial.commits[0].numberfiles) 
+        
+    def test_commits_per_author(self):
+        self.assertEqual(26, self.trial.authors['Vincent'])
+        self.assertEqual(422, sum(self.trial.authors.values()))
+        
+    def test_files_per_author(self):
+        self.assertEqual(337, self.trial.numberfiles['Vincent'])
+        self.assertEqual(3011, sum(self.trial.numberfiles.values()))
+         
+    def test_amd(self):
+        self.assertEqual(260, self.trial.numberA['Vincent'])
+        self.assertEqual(45, self.trial.numberM['Vincent'])
+        self.assertEqual(32, self.trial.numberD['Vincent'])        
+        self.assertEqual(3009, (sum(self.trial.numberA.values())+sum(self.trial.numberM.values())+sum(self.trial.numberD.values())))#two R files so 3011-2 expected
+        
+        
+        
      
 if __name__ == '__main__':
     unittest.main()
